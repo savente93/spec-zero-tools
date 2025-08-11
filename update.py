6bd7befb97823d648a932c133d1350ba4b682556
Sam Vente <savente93@proton.me>
2025-08-11 14:24:06 +0200

    feat: add main update script

---
+++ update.py
@@ -0,0 +1,26 @@
+from spec_zero_tools import update_pyproject_toml, read_toml, write_toml, read_schedule
+from pathlib import Path
+from argparse import ArgumentParser
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(
+                        prog='spec_zero_update',
+                        description='A script to update your project dependencies to be in line with the scientific python SPEC 0 support schedule',
+                        )
+
+    parser.add_argument('toml_path', default="pyproject.toml", help="Path to the project file that lists the dependencies. defaults to 'pyproject.toml'.")           
+
+    args = parser.parse_args()
+
+    toml_path = Path(args.toml_path)
+
+    if not toml_path.exists():
+        raise ValueError(f"{toml_path} was supplied as path to project file but it did not exist")
+
+    project_data = read_toml(toml_path)
+    schedule_data = read_schedule("schedule.json")
+    update_pyproject_toml(project_data, schedule_data)
+
+    write_toml(toml_path, project_data)
