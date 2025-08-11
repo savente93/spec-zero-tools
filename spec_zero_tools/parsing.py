e6912aad2d32490a03bc3963c64e897d31a7ab48
Sam Vente <savente93@proton.me>
2025-08-11 14:23:29 +0200

    feat: add python module with helper functions and tests

---
+++ spec_zero_tools/parsing.py
@@ -0,0 +1,79 @@
+from typing import TypeAlias
+from urllib.parse import ParseResult, urlparse
+from tomlkit import dumps, loads
+import json
+from packaging.specifiers import InvalidSpecifier, SpecifierSet
+from packaging.version import InvalidVersion, Version
+from typing import Dict, Sequence, Tuple, TypedDict
+from pathlib import Path
+from re import compile
+
+# we won't actually do anything with URLs we just need to detect them
+Url: TypeAlias = ParseResult
+
+# slightly modified version of https://packaging.python.org/en/latest/specifications/dependency-specifiers/#names
+PEP_PACKAGE_IDENT_RE = compile(r"(?im)^([A-Z0-9][A-Z0-9._-]*[A-Z0-9])(.*)$")
+
+
+class SupportSchedule(TypedDict):
+    start_date: str
+    packages: Dict[str, str]
+
+
+def parse_version_spec(s: str) -> SpecifierSet:
+    if s.strip() == "*":
+        # python version numeric components must be non-negative so this is ookay
+        # see https://packaging.python.org/en/latest/specifications/version-specifiers/
+        return SpecifierSet(">=0")
+    try:
+        # if we can simply parse it return it
+        return SpecifierSet(s)
+    except InvalidSpecifier:
+        try:
+            ver = Version(s)
+        except InvalidVersion:
+            raise ValueError(f"{s} is not a version or specifyer")
+
+        return SpecifierSet(f">={ver}")
+
+
+def write_toml(path: Path | str, data: dict):
+    with open(path, "w") as file:
+        contents = dumps(data)
+        file.write(contents)
+
+
+def read_toml(path: Path | str) -> dict:
+    with open(path, "r") as file:
+        contents = file.read()
+        return loads(contents)
+
+
+def read_schedule(path: Path | str) -> Sequence[SupportSchedule]:
+    with open(path, "r") as file:
+        return json.load(file)
+
+
+def parse_pep_dependency(dep_str: str) -> Tuple[str, SpecifierSet | Url | None]:
+    match = PEP_PACKAGE_IDENT_RE.match(dep_str)
+    if match is None:
+        raise ValueError("Could not find any valid python package identifier")
+
+    match_groups = match.groups()
+
+    pkg = match_groups[0]
+    # capture group could be empty
+    if len(match_groups) > 1 and match_groups[1]:
+        spec_str = match_groups[1]
+        if is_url_spec(spec_str):
+            spec = urlparse(spec_str.split("@")[1])
+        else:
+            spec = SpecifierSet(spec_str)
+    else:
+        spec = None
+
+    return (pkg, spec)
+
+
+def is_url_spec(str_spec: str) -> bool:
+    return str_spec.strip().startswith("@")
---
+++ tests/test_parsing.py
@@ -0,0 +1,55 @@
+from spec_zero_tools.parsing import parse_version_spec, parse_pep_dependency
+from packaging.specifiers import SpecifierSet
+import pytest
+from urllib.parse import urlparse
+
+
+def test_parsing_correct():
+    assert SpecifierSet(">=0") == parse_version_spec("*")
+    assert SpecifierSet(">4,<9") == parse_version_spec(">4, <9")
+    assert SpecifierSet(">=4") == parse_version_spec(">=4")
+    assert SpecifierSet(">=2025.7") == parse_version_spec(">=2025.7")
+
+
+def test_parsing_incorrect():
+    with pytest.raises(ValueError):
+        parse_version_spec("-18")
+
+    with pytest.raises(ValueError):
+        parse_version_spec("asdf")
+
+
+def test_pep_dependency_parsing():
+    matplotlib_str = "matplotlib"
+    pkg, spec = parse_pep_dependency(matplotlib_str)
+
+    assert pkg == "matplotlib", pkg
+
+    assert spec is None, spec
+
+
+def test_pep_dependency_parsing_with_spec():
+    matplotlib_str = "matplotlib>=3.7.0,<4"
+    pkg, spec = parse_pep_dependency(matplotlib_str)
+
+    assert pkg == "matplotlib", pkg
+    assert spec == SpecifierSet(">=3.7.0,<4"), spec
+
+
+def test_pep_dependency_parsing_with_url_spec():
+    dep_str = "matplotlib @ https://github.com/pypa/pip/archive/1.3.1.zip#sha1=da9234ee9982d4bbb3c72346a6de940a148ea686"
+    pkg, spec = parse_pep_dependency(dep_str)
+
+    assert pkg == "matplotlib", pkg
+    assert spec == urlparse(
+        " https://github.com/pypa/pip/archive/1.3.1.zip#sha1=da9234ee9982d4bbb3c72346a6de940a148ea686"
+    ), spec
+
+
+def test_pep_dependency_parsing_extra_restrictions():
+    matplotlib_str = "matplotlib>=3.7.0,<4,!=3.8.14"
+    pkg, spec = parse_pep_dependency(matplotlib_str)
+
+    assert pkg == "matplotlib", pkg
+
+    assert spec == SpecifierSet("!=3.8.14,<4,>=3.7.0"), spec
